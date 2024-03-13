6a53fd84e8f4c64182587354ad00f81e07c099fb
Ben Prejean <benprejean@live.com>
2024-03-13 07:47:55 -0400

    switch to azure ad auth

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ terraform {
     storage_account_name = "tfgithubactionsbp"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true  
+    use_azuread_auth     = true
+    #use_oidc             = true  
   }
 }
 
